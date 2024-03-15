246d29047a6ce3046aca3ac584546845198fcd86
molasz <martiolasz@gmail.com>
2024-03-15 19:47:40 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  }
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
